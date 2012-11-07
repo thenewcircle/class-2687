5e06c0a0ec59b6f9e0449c5f080284f37548ee1d
Marko Gargenta <marko@marakana.com>
2012-11-07 11:12:22 -0800

    Implemented insert()

--- MYamba/src/com/verizon/myamba/RefreshService.java
+++ MYamba/src/com/verizon/myamba/RefreshService.java
@@ -3,6 +3,7 @@ package com.verizon.myamba;
 import java.util.List;
 
 import android.app.IntentService;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.util.Log;
 
@@ -14,10 +15,12 @@ public class RefreshService extends IntentService {
 	private static final String TAG = "RefreshService";
 	private static final int MAX_POSTS = 20;
 	private YambaClient client;
+	private ContentValues values;
 
 	public RefreshService() {
 		super(TAG);
 		client = new YambaClient("student", "password");
+		values = new ContentValues();
 	}
 
 	// Runs on a worker thread
@@ -32,6 +35,14 @@ public class RefreshService extends IntentService {
 				Log.d(TAG,
 						String.format("%s: %s", status.getUser(),
 								status.getMessage() ));
+				
+				// Insert into db
+				values.clear();
+				values.put( StatusContract.Columns._ID, status.getId() );
+				values.put( StatusContract.Columns.USER, status.getUser() );
+				values.put( StatusContract.Columns.MESSAGE, status.getMessage() );
+				values.put( StatusContract.Columns.CREATED_AT, status.getCreatedAt().getTime() );
+				getContentResolver().insert( StatusContract.CONTENT_URI, values);
 			}
 		} catch (YambaClientException e) {
 			e.printStackTrace();
---
+++ MYamba/src/com/verizon/myamba/StatusProvider.java
@@ -0,0 +1,85 @@
+package com.verizon.myamba;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+public class StatusProvider extends ContentProvider {
+	private static final String TAG = "StatusProvider";
+
+	private static final int STATUS_ITEM = 1;
+	private static final int STATUS_DIR = 2;
+	private static UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+	static {
+		uriMatcher.addURI(StatusContract.AUTHORITY, StatusContract.PATH,
+				STATUS_DIR);
+		uriMatcher.addURI(StatusContract.AUTHORITY, StatusContract.PATH + "/#",
+				STATUS_ITEM);
+	}
+
+	private DbHelper dbHelper;
+	private SQLiteDatabase db;
+
+	@Override
+	public boolean onCreate() {
+		dbHelper = new DbHelper(getContext());
+		return (dbHelper == null) ? false : true;
+	}
+
+	// uri: content://com.verizon.yamba.provider/status
+	@Override
+	public String getType(Uri uri) {
+		switch (uriMatcher.match(uri)) {
+		case STATUS_DIR:
+			return StatusContract.CONTENT_TYPE;
+		case STATUS_ITEM:
+			return StatusContract.CONTENT_ITEM_TYPE;
+		default:
+			return null;
+		}
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+		// Assert
+		if (uriMatcher.match(uri) != STATUS_DIR) {
+			throw new IllegalArgumentException("Unsupported uri: " + uri);
+		}
+
+		// Insert into the database
+		db = dbHelper.getWritableDatabase();
+		long id = db.insertWithOnConflict(StatusContract.PATH, null, values,
+				SQLiteDatabase.CONFLICT_IGNORE);
+
+		// Return value
+		if (id > 0) {
+			return ContentUris.withAppendedId(uri, id);
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection,
+			String[] selectionArgs, String sortOrder) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection,
+			String[] selectionArgs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
